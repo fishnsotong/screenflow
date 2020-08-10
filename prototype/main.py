b7b5ce14228931147ce6ddc6eccb2b56b257e277
Wayne Yeo <fishnsotong@gmail.com>
2020-08-10 09:40:22 +0800

    prototyping notebooks

---
+++ prototype/main.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# File              : main.py
+# Author            : Wayne Yeo [fishnsotong@gosigs] <wwzyeo@gmail.com>
+# Date              : 2020-06-29T07:48:48+0800
+# Last Modified Date: 2020-06-29T08:03:02+0800
+# Last Modified By  : Wayne Yeo [fishnsotong@gosigs] <wwzyeo@gmail.com>
+
+import cv2
+import argparse
+import libdmtx
+
+ap = argparse.ArgumentParser()
+
+def decoder():
+    return dc
+
+if "__name__" == __main__:
+    ap.add_argument("-i", "--image", required = True, help = 'path to input image')
